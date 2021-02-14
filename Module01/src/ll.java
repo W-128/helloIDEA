92685be81e44f3f31c6ad55303f546ac045b1727
wyz_glb <wyz@atguigu.com>
2021-02-14 18:03:20 +0800

    Initial commit

---
+++ Module01/src/ll.java
@@ -0,0 +1,6 @@
+/**
+ * @author wuyunzhi
+ * @create 2021-02-14 16:02
+ */
+public class ll {
+}
---
+++ src/TemplateTest.java
@@ -0,0 +1,30 @@
+/**
+ * @author wuyunzhi
+ * @create 2021-02-14 16:29
+ */
+public class TemplateTest {
+    //psvm
+    public static void main(String[] args) {
+        //sout
+        System.out.println("hello");
+        //soutp
+        System.out.println("args = [" + args + "]");
+        //soutm
+        System.out.println("TemplateTest.main");
+        //soutv
+        int num1 = 10;
+        System.out.println("num1 = " + num1);
+        //XXX.sout
+        System.out.println(num1);
+        
+        //fori
+        for (int i = 0; i < 5; i++) {
+            
+        }
+        //iter
+        for (String arg : args) {
+            
+        }
+    }
+
+}
